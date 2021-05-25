0df88cf264acb8c14ff6dcfd5dfaf6e2cc75ea57
dianjixz <18637716021@163.com>
2021-05-25 15:09:33 +0800

    更新一些文件

---
+++ scripts/img_make/tempCodeRunnerFile.sh
@@ -0,0 +1,21 @@
+while getopts ":d:f:bh" OPT;do
+	case $OPT in
+		  d)
+             	echo "asdasd"
+             	;;
+          f)    echo "The options is b."
+                echo "OPTARG:$OPTARG"
+                echo "OPTIND:$OPTIND"
+                ;;
+
+		  b)
+				echo "asd"
+				help
+		  		;;
+		  h)    help
+		  		
+				;;
+		  *)	help
+				;;
+	esac
+done
---
+++ scripts/sdX_temp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+while [ 1 ]
+do
+
+
+
+
+sudo smartctl -a /dev/nvme0n1
+sleep 0.5
+
+
+done
+
+
+
