e671bafffddd7c7dac50cebb0208309fd6627b01
dianjixz <18637716021@163.com>
2022-06-25 00:21:19 +0800

    [update]

---
+++ lib_example/python/get_ip.py
@@ -0,0 +1,14 @@
+#!/bin/env python3
+import socket
+def extract_ip():
+    st = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:       
+        st.connect(('10.255.255.255', 1))
+        IP = st.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        st.close()
+    return IP
+
+print(extract_ip())
