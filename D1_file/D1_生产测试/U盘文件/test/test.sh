8fdd75f5ccd0f074af0e028bac9d7e3ea3dc039c
dianjixz <18637716021@163.com>
2023-05-12 11:28:34 +0800

    [update]

--- D1_file/D1_生产测试/U盘文件/test/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-/mnt/test/can-test >> /dev/null 2>&1 &
-
-/mnt/test/uart-test /dev/ttyS2 >> /dev/null 2>&1 &
-
-/mnt/test/uart-test /dev/ttyS3 >> /dev/null 2>&1 &
--- zero_sd_img/read.md
+++
@@ -1,5 +0,0 @@
-used!
-
-
-
-sudo dd if=sd.img of=/dev/sdX bs=1024 seek=8
